2f47210d0359fef36bf4ac46aeadc8b77ebb79e0
veryfatwombat <veryfatwombat@gmail.com>
2021-03-23 00:16:45 +1100

    js

--- en/js/en.js
+++ en/js/en.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
@@ -20,3 +22,44 @@ navClose.addEventListener("click", () => {
     navBar.classList.remove("show");
   }
 });
+
+const navHeight = navBar.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+const links = [...document.querySelectorAll(".scroll-link")];
+
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const el = document.getElementById(id);
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  });
+});
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: 200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
