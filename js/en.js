1dfd6ab9c3d55bba4de6bc495d6b4249d812aaa9
veryfatwombat <veryfatwombat@gmail.com>
2021-03-24 01:25:57 +1100

    js

--- js/en.js
+++ js/en.js
@@ -3,7 +3,7 @@
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
-const navBar = document.querySelector(".nav");
+// const navBar = document.querySelector(".nav");
 
 const navLeft = menu.getBoundingClientRect().left;
 
@@ -23,6 +23,7 @@ navClose.addEventListener("click", () => {
   }
 });
 
+const navBar = document.querySelector(".nav");
 const navHeight = navBar.getBoundingClientRect().height;
 
 window.addEventListener("scroll", () => {
@@ -43,23 +44,29 @@ links.map((link) => {
 
     const id = e.target.getAttribute("href").slice(1);
 
-    const el = document.getElementById(id);
-    let position = el.offsetTop - navHeight;
+    //const el = document.getElementById(id);
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    //let position = el.offsetTop - navHeight;
+    let position = element.offsetTop - navHeight;
 
     window.scrollTo({
       top: position,
       left: 0,
     });
 
+    //menu.classList.remove("show");
+    //document.body.classList.remove("show");
+    //navBar.classList.remove("show");
+    navBar.classList.remove("show");
     menu.classList.remove("show");
     document.body.classList.remove("show");
-    navBar.classList.remove("show");
   });
 });
 
 gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
 gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
-gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: 200 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
 gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
-gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
 gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
